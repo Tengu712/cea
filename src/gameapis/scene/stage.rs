af73e1b969d03f24bc418ccdc3728dd980b0184c
Tengu712 <genreihoutengu@gmail.com>
2022-04-22 13:18:13 +0900

    draw score and enemy

--- src/gameapis/scene/stage.rs
+++ src/gameapis/scene/stage.rs
@@ -3,17 +3,27 @@ use super::*;
 const P_SPD: f32 = 8.0;
 
 pub struct Stage {
+    score: u64,
+    count: u32,
+    e_hp: [u32; 2],
+    e_pos_xy: [f32; 2],
     p_pos_xy: [f32; 2],
 }
 impl Stage {
     pub fn new() -> Self {
-        Self { p_pos_xy: [0.0; 2] }
+        Self {
+            score: 0,
+            count: 0,
+            e_hp: [2000; 2],
+            e_pos_xy: [0.0, 280.0],
+            p_pos_xy: [0.0, -280.0],
+        }
     }
     pub fn update(self, keystates: &KeyStates) -> (Scene, LinkedList<Request>) {
+        // Update player
         let inp_x = (keystates.right > 0) as i32 - (keystates.left > 0) as i32;
         let inp_y = (keystates.up > 0) as i32 - (keystates.down > 0) as i32;
-        let c_spd = if keystates.s > 0 { 0.5 } else { 1.0 };
-        let c_spd = c_spd
+        let c_spd = if keystates.s > 0 { 0.5 } else { 1.0 }
             / if inp_x.abs() + inp_y.abs() == 2 {
                 std::f32::consts::SQRT_2
             } else {
@@ -23,7 +33,28 @@ impl Stage {
             self.p_pos_xy[0] + inp_x as f32 * P_SPD * c_spd,
             self.p_pos_xy[1] + inp_y as f32 * P_SPD * c_spd,
         ];
+        // Update enemy
+        let e_pos_xy = [
+            self.e_pos_xy[0],
+            self.e_pos_xy[1] + (self.count as f32 * 6.0).to_radians().cos() * 0.5,
+        ];
+        let e_hp = self.e_hp;
+        // Update count
+        let count = self.count + 1;
+        // Update score
+        let score = self.score + 10;
+        //
+        // Build request list
         let mut reqs = LinkedList::new();
+        // Enemy
+        reqs.push_back(
+            CDataDiff::new()
+                .set_trs(e_pos_xy)
+                .set_scl([100.0, 100.0])
+                .pack(),
+        );
+        reqs.push_back(Request::DrawImage);
+        // Player
         reqs.push_back(
             CDataDiff::new()
                 .set_trs(p_pos_xy)
@@ -31,6 +62,23 @@ impl Stage {
                 .pack(),
         );
         reqs.push_back(Request::DrawImage);
-        (Scene::Stage(Self { p_pos_xy }), reqs)
+        // Score
+        reqs.push_back(
+            TextDesc::new()
+                .set_text(format!("{:>012}", score))
+                .set_rect([300.0, 1280.0, 0.0, 960.0])
+                .set_format(TextFormat::Score)
+                .pack(),
+        );
+        (
+            Scene::Stage(Self {
+                score,
+                count,
+                e_hp,
+                e_pos_xy,
+                p_pos_xy,
+            }),
+            reqs,
+        )
     }
 }
